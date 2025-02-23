3cd53882cda92ac526ff514aa09a393944f4342b
w-foster <william.jfoster@yahoo.com>
2025-02-23 22:17:46 +0000

    add cors for sqs integration

--- main-api/app.py
+++ main-api/app.py
@@ -2,21 +2,32 @@ from flask import Flask, request, jsonify
 import boto3
 import json
 import os
-import uiud
+import uuid
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 # Init SQS client using boto3 package
 sqs = boto3.client('sqs', region_name=os.getenv('AWS_REGION', 'eu-west-2'))
 
 SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL', 'https://sqs.eu-west-2.amazonaws.com/202533540156/CodeQueue.fifo')
 
+@app.route('/')
+def index():
+    return "hello"
+
 @app.route('/submit-code', methods=['POST'])
 def submit_code():
+    print('entering submit code')
     data = request.get_json()
     code = data.get('code')
     problem_id = data.get('problem_id')
     language = data.get('language')
+    test_cases = data.get('test_cases')
 
     if not code or not problem_id or not language:
         return jsonify({'error': 'Missing required parameters'}), 400
@@ -36,10 +47,15 @@ def submit_code():
     try:
         response = sqs.send_message(
             QueueUrl=SQS_QUEUE_URL,
-            MessageBody=message_body
+            MessageBody=message_body,
+            MessageGroupId='default'
         )
+        print('sent...')
         return jsonify({'message_id': response.get('MessageId'), 'status': 'queued'}), 200
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
