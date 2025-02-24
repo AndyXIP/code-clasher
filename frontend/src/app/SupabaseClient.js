73060f6c549058697a81e8fa32c64924cf0750ff
Lucas Chan <150052194+lucas120301@users.noreply.github.com>
2025-02-24 23:12:30 +0000

    changed supabase client to new database

--- frontend/src/app/SupabaseClient.js
+++ frontend/src/app/SupabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://qgquqfmmesugnhnwdyel.supabase.co';
-const SUPABASE_ANON_PUBLIC_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncXVxZm1tZXN1Z25obndkeWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzMjY0MjEsImV4cCI6MjA1NTkwMjQyMX0.xbU9WjpuUx7UUBN2TAaaJmgfuYp12sgnRb-MVNDUEG8'
+const SUPABASE_URL = 'https://ameuebynkhydqqsvpjau.supabase.co';
+const SUPABASE_ANON_PUBLIC_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZXVlYnlua2h5ZHFxc3ZwamF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MzgyNjUsImV4cCI6MjA1NjAxNDI2NX0.sb23Tc10jGK_PV8dRxrAsHMhbhX36-R_pAmuXpPZ1Q8'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_PUBLIC_KEY);
