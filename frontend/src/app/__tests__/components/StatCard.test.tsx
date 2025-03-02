7e93fc510d70b2a419573fd0f94f043681678bd8
Andy <80457130+AndyXIP@users.noreply.github.com>
2025-03-02 18:22:48 +0000

    Update StatCard.test.tsx
    Fix component rendering and checking

--- frontend/src/app/__tests__/components/StatCard.test.tsx
+++ frontend/src/app/__tests__/components/StatCard.test.tsx
@@ -16,23 +16,19 @@ describe('StatCard Component', () => {
     render(<StatCard />);
 
     // Wait for all the stats to render
-    const stat1Name = await screen.findByText('Total Questions Completed');
-    const stat1Value = await screen.findByText('5');
-
-    const stat2Name = await screen.findByText('Question Streak');
-    const stat2Value = await screen.findByText('3');
-
-    const stat3Name = await screen.findByText('Questions Completion Rate');
-    const stat3Value = await screen.findByText('80%');
+    const stat1Name = await screen.findByText('Easy Questions Completed');
+    const stat2Name = await screen.findByText('Hard Questions Completed');
+    const stat3Name = await screen.findByText('Total Questions Completed');
+    const stat4Name = await screen.findByText('Easy Questions Completion Rate');
+    const stat5Name = await screen.findByText('Hard Questions Completion Rate');
+    const stat6Name = await screen.findByText('Total Questions Completion Rate');
 
     // Assert that each stat is present in the document
     expect(stat1Name).toBeInTheDocument();
-    expect(stat1Value).toBeInTheDocument();
-
     expect(stat2Name).toBeInTheDocument();
-    expect(stat2Value).toBeInTheDocument();
-
     expect(stat3Name).toBeInTheDocument();
-    expect(stat3Value).toBeInTheDocument();
+    expect(stat4Name).toBeInTheDocument();
+    expect(stat5Name).toBeInTheDocument();
+    expect(stat6Name).toBeInTheDocument();
   });
 });
